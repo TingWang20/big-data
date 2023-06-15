b31e7a32b6f825bde05e99d1da5bf84d8f0fa6a4
benson <lee07benson@outlook.com>
2023-06-15 13:45:20 +0800

    init and upload file

---
+++ python/debug/error_n_method.py
@@ -0,0 +1,4 @@
+# 1 - model not found error：no module named 找不到自定义模块
+# >> method1: 导入的路径，文件名是否正确
+# >> method2: 在报错的模块文件中添加路径。sys.path.append("..") 或者 sys.path.append("这里放导入模块的绝对路径")
+# >> method3: 删除运行python文件所在文件夹里的编译文件
